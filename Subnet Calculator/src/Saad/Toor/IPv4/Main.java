89acd6a2017b3269b9fa489fc53ba65ce1afe87f
SaadToor <saad.321315475@hotmail.com>
2017-01-23 20:21:46 -0800

    Only reading from the file for IPv4 address Outputing the address with a question.

---
+++ Subnet Calculator/src/Saad/Toor/IPv4/Main.java
@@ -0,0 +1,15 @@
+package Saad.Toor.IPv4;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String args[]){
+		String fileName = "IPv4.txt";
+		
+		IPv4 ipv4 = new IPv4(fileName);
+		
+		System.out.println();
+		System.out.println("What will like to do? ");
+	}
+}
